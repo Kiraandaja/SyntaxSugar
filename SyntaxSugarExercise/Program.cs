b4d7460506c4d10434bd1847fd0871eeaf49b85c
Kiraandaja <kiraandaja@gmail.com>
2022-04-28 12:37:23 -0700

    Initial Commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4; //Explicit Typing
+            string response;//Explicit Typing
+
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+
+                Console.WriteLine(response);
+            }
+            else
+            {
+                response = answer + "greater than or equal to nine";
+                Console.WriteLine(response);
+            }
+            //Terenary Operator, Syntax Sugar, String Interpolation
+            var response2 = (answer < 9) ? $"{answer} is less than nine." : $"{answer} is greater than or equal to nine.";
+
+
+        }
+        
+
+
+
+    }
+}
